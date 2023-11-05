88e95c03163d4c3a1c040e05977328a1f977c242
waimengkok <149142167+waimengkok@users.noreply.github.com>
2023-11-05 02:58:43 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,81 +1,68 @@
-// Create Restful Api for Comments
-// 1. Create Comment
-// 2. Get All Comments
-// 3. Get Comment by Id
-// 4. Update Comment
-// 5. Delete Comment
+// Create web server application with Express.js
+// To run: node comments.js
 
-// Import Express
-const express = require('express');
-// Import Router
-const router = express.Router();
-// Import Comment Model
-const Comment = require('../models/comment');
-// Import Post Model
-const Post = require('../models/post');
+const express = require("express");
+const app = express();
 
-// Create Comment
-router.post('/comments', (req, res) => {
-    // Create Comment
-    Comment.create(req.body, (err, comment) => {
-        if (err) {
-            res.status(400).json({ message: err.message });
-        } else {
-            // Find Post by Id
-            Post.findById(req.body.postId, (err, post) => {
-                if (err) {
-                    res.status(400).json({ message: err.message });
-                } else {
-                    // Push Comment to Post
-                    post.comments.push(comment);
-                    // Save Post
-                    post.save();
-                    // Response
-                    res.status(200).json({ message: 'Comment Created Successfully' });
-                }
-            });
-        }
-    });
+// Set up the port number
+const port = 3000;
+
+// Set up the route for the root
+app.get("/", (req, res) => {
+  res.send("Welcome to the comments web service");
 });
 
-// Get All Comments
-router.get('/comments', (req, res) => {
-    // Find All Comments
-    Comment.find({}, (err, comments) => {
-        if (err) {
-            res.status(400).json({ message: err.message });
-        } else {
-            // Response
-            res.status(200).json(comments);
-        }
-    });
+// Set up the route for the comments
+app.get("/comments", (req, res) => {
+  res.send("Welcome to the comments web service");
 });
 
-// Get Comment by Id
-router.get('/comments/:id', (req, res) => {
-    // Find Comment by Id
-    Comment.findById(req.params.id, (err, comment) => {
-        if (err) {
-            res.status(400).json({ message: err.message });
-        } else {
-            // Response
-            res.status(200).json(comment);
-        }
-    });
+// Set up the route for the comments with a parameter
+app.get("/comments/:year/:month", (req, res) => {
+  res.send(req.params);
 });
 
-// Update Comment
-router.patch('/comments/:id', (req, res) => {
-    // Find Comment by Id and Update
-    Comment.findByIdAndUpdate(req.params.id, req.body, (err, comment) => {
-        if (err) {
-            res.status(400).json({ message: err.message });
-        } else {
-            // Response
-            res.status(200).json({ message: 'Comment Updated Successfully' });
-        }
-    });
+// Set up the route for the comments with a query string
+app.get("/comments/:year/:month", (req, res) => {
+  res.send(req.query);
 });
 
-// Delete Comment
-router.delete('/comments');
+// Set up the route for the comments with a query string
+app.get("/comments/:year/:month", (req, res) => {
+  res.send(req.query);
+});
+
+// Set up the route for the comments with a query string
+app.get("/comments/:year/:month", (req, res) => {
+  res.send(req.query);
+});
+
+// Set up the route for the comments with a query string
+app.get("/comments/:year/:month", (req, res) => {
+  res.send(req.query);
+});
+
+// Set up the route for the comments with a query string
+app.get("/comments/:year/:month", (req, res) => {
+  res.send(req.query);
+});
+
+// Set up the route for the comments with a query string
+app.get("/comments/:year/:month", (req, res) => {
+  res.send(req.query);
+});
+
+// Set up the route for the comments with a query string
+app.get("/comments/:year/:month", (req, res) => {
+  res.send(req.query);
+});
+
+// Set up the route for the comments with a query string
+app.get("/comments/:year/:month", (req, res) => {
+  res.send(req.query);
+});
+
+// Listen on the port
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
